272742214327f705954a96f602e65d82ffbefba1
Noah <spiritand564@gmail.com>
2025-06-29 20:19:10 -0400

    Implement Sponge scheduling

--- sponge/src/main/java/better/anticheat/sponge/SpongeDataBridge.java
+++ sponge/src/main/java/better/anticheat/sponge/SpongeDataBridge.java
@@ -3,12 +3,17 @@ package better.anticheat.sponge;
 import better.anticheat.core.DataBridge;
 import com.github.retrooper.packetevents.protocol.player.User;
 import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.api.Game;
 import org.spongepowered.api.Sponge;
 import org.spongepowered.api.SystemSubject;
 import org.spongepowered.api.command.exception.CommandException;
 import org.spongepowered.api.entity.living.player.server.ServerPlayer;
+import org.spongepowered.api.scheduler.ScheduledTask;
+import org.spongepowered.api.scheduler.Task;
+import org.spongepowered.api.util.Ticks;
 
+import java.io.Closeable;
 import java.util.Optional;
 
 public class SpongeDataBridge implements DataBridge {
@@ -50,5 +55,21 @@ public class SpongeDataBridge implements DataBridge {
             String message = "Cannot process command \"" + command + "\"!";
             logger.warn(message);
         }
+    }
+
+    @Override
+    public @Nullable Closeable registerTickListener(User user, Runnable runnable) {
+        Task.Builder builder = Task.builder();
+        Task task = builder.execute(runnable).interval(Ticks.of(1)).build();
+        ScheduledTask scheduledTask = game.server().scheduler().submit(task);
+        return scheduledTask::cancel;
+    }
+
+    @Override
+    public @Nullable Closeable runTaskLater(User user, Runnable runnable, int delayTicks) {
+        Task.Builder builder = Task.builder();
+        Task task = builder.execute(runnable).delay(Ticks.of(delayTicks)).build();
+        ScheduledTask scheduledTask = game.server().scheduler().submit(task);
+        return scheduledTask::cancel;
     }
 }
