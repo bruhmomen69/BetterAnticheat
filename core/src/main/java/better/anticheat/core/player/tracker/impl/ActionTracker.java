30a25512ebe04f50692f3c14410442ab8ae08a59
bruhmomen69 <support@haldor.xyz>
2025-07-23 13:46:06 +1200

    begin work on better mitigations

--- core/src/main/java/better/anticheat/core/player/tracker/impl/ActionTracker.java
+++ core/src/main/java/better/anticheat/core/player/tracker/impl/ActionTracker.java
@@ -4,15 +4,60 @@ import better.anticheat.core.player.Player;
 import better.anticheat.core.player.tracker.Tracker;
 import com.github.retrooper.packetevents.event.simple.PacketPlayReceiveEvent;
 import com.github.retrooper.packetevents.event.simple.PacketPlaySendEvent;
-import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientEntityAction;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPlayerBlockPlacement;
 import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPlayerDigging;
-import com.github.retrooper.packetevents.wrapper.play.client.WrapperPlayClientPlayerFlying;
+import org.jetbrains.annotations.Nullable;
+import wtf.spare.sparej.incrementer.IntIncrementer;
+
+import com.github.retrooper.packetevents.protocol.item.ItemStack;
+import com.github.retrooper.packetevents.protocol.player.InteractionHand;
+import com.github.retrooper.packetevents.protocol.world.BlockFace;
+import com.github.retrooper.packetevents.util.Vector3f;
+import com.github.retrooper.packetevents.util.Vector3i;
+
+import java.util.Optional;
 
 public class ActionTracker extends Tracker {
     public ActionTracker(Player player) {
         super(player);
     }
+
+    private final IntIncrementer digState = new IntIncrementer(0);
+    // Digging
+    private boolean digging = false;
+    private BlockFace diggingBlockFace = BlockFace.OTHER;
+    private @Nullable Vector3i diggingPosition = null;
+
+    // Placing
+    private BlockFace placingBlockFace = BlockFace.OTHER;
+    private BlockFace lastPlacingBlockFace = BlockFace.OTHER;
+    private @Nullable Vector3i lastPlacePosition = null;
+    private @Nullable Vector3i placePosition = null;
+    private @Nullable Vector3f lastCursorPosition = null;
+    private @Nullable Vector3f cursorPosition = null;
+    private @Nullable InteractionHand lastPlaceHand = null;
+    private @Nullable InteractionHand placeHand = null;
+    private Optional<ItemStack> lastPlaceItem = Optional.empty();
+    private Optional<ItemStack> placeItem = Optional.empty();
+    private boolean isPlacingInsideBlock = false;
+
+    // Sprint/Sneak
+    private boolean sneaking = false, wasSneaking = false;
+    private boolean sprinting = false, wasSprinting = false;
+
+    // Smart collision values
+    private boolean crouching = false, wasCrouching = false;
+
+    // Ticks Since
+    private final IntIncrementer ticksSinceSwing = new IntIncrementer();
+    private final IntIncrementer ticksSinceCombatSwing = new IntIncrementer();
+    private final IntIncrementer ticksSinceAttack = new IntIncrementer();
+    private final IntIncrementer ticksSinceDigging = new IntIncrementer();
+    private final IntIncrementer ticksSinceSuccessfulDig = new IntIncrementer();
+    private final IntIncrementer ticksSincePlace = new IntIncrementer();
+    private final IntIncrementer ticksSinceRealPlace = new IntIncrementer();
+    private final IntIncrementer ticksSinceRealPlaceUnder = new IntIncrementer();
+    private final IntIncrementer ticksSinceTeleport = new IntIncrementer();
 
     @Override
     public void handlePacketPlayReceive(PacketPlayReceiveEvent event) {
@@ -136,5 +181,6 @@ public class ActionTracker extends Tracker {
     }
 
     @Override
-    public void handlePacketPlaySend(PacketPlaySendEvent event) {}
+    public void handlePacketPlaySend(PacketPlaySendEvent event) {
+    }
 }
